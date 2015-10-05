0caf7f6f324524998ce2ec0b4a2dc2e826c8eba2
axelgafu <axel.gafu@gmail.com>
2015-10-04 23:08:00 -0500

    20151004: Added Category consntants.

--- MoSynAPI/src/base/AbstractMorphology.py
+++ MoSynAPI/src/base/AbstractMorphology.py
@@ -11,6 +11,20 @@ class AbstractMorphology(object):
     '''
     classdocs
     '''
+    CAT_UNKNOWN       = 0
+    CAT_ADJETIVO      = 1
+    CAT_ADVERBIO      = 2 
+    CAT_ARTICULOS     = 3
+    CAT_DETERMINANTES = 4
+    CAT_NOMBRES       = 5
+    CAT_VERBOS        = 6
+    CAT_PRONOMBRES    = 7
+    CAT_CONJUNCIONES  = 8
+    CAT_NUMERALES     = 9
+    CAT_INTERJECCIONES= 10
+    CAT_ABREVIATURAS  = 11
+    CAT_PREPOSICIONES = 12
+    CAT_PUNTUACION    = 13
 
 
     def __init__(self, params):
@@ -48,3 +62,20 @@ class AbstractMorphology(object):
     '''
     def getEaglesLabel(self):
         raise NotImplementedError()
+    
+    
+    
+    '''
+    ' Returns the category of the word based on the Eagles label; e.g.:
+    '    Eagles Label: AQ0CP00
+    '    Category:     CAT_ADJETIVO
+    '
+    ' If the category cannot be determined then CAT_UNKNOWN is returned.
+    ' return Integer
+    '''
+    def getCategory(self):
+        raise NotImplementedError()
+    
+    '''
+    '' Several methods pending to be defined. 
+    '''
