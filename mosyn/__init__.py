98d2e4f198776d3d1041bf80697a72bc7644fab4
torreart <art.torres.alv@gmail.com>
2015-11-15 15:50:57 -0600

    Initial release

---
+++ mosyn/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+# Created on 10/11/2015
+# Modified on 15/11/2015
+# Modified by asaelt
+#
+# source: https://github.com/uagdataanalysis/mosynapi
+#
+# @author: asaelt
