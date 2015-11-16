4d071ce1bf1cf04ea6934b62778cb79ba1e64792
torreart <art.torres.alv@gmail.com>
2015-11-16 07:50:24 -0600

    Se agrega funcionalidad para la execución desde el directorio.

--- mosyn/__main__.py
+++ mosyn/__main__.py
@@ -1,1 +1,17 @@
-__author__ = 'asaelt'
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+# Created on 14/11/2015
+# Modified on 14/11/2015
+# Modified by asaelt
+#
+# source: https://github.com/uagdataanalysis/mosynapi
+#
+# @author: asaelt
+
+import sys
+
+from mosyn import main
+
+
+main(sys.argv)
