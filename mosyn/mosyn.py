cd71b6b8dfcbfcc2f62bf39a1cca013af64f4b76
torrar <art.torres.alv@gmail.com>
2018-07-21 07:22:20 -0500

    Set version 1.0.8

--- mosyn/mosyn.py
+++ mosyn/mosyn.py
@@ -22,7 +22,7 @@ from util.io import SystemInput
 from util.io import SystemOutput
 from util.events import EventHook
 
-__version__ = '1.0.6'
+__version__ = '1.0.8'
 
 
 class MorphologicalAnalysis:
